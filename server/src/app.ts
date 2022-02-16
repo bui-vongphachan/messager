a27632c197eb60b9a96baf3ed49f86016d256879
vongphachan nukkalash <vongphachannukkalash@gmail.com>
2022-02-16 10:11:00 +0700

    ແກ້ req.user ບໍ່ເຈີໃນ facebook callback url

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,10 +12,7 @@ app.use(bodyParser.json());
 
 app.use(
   session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true },
+    secret: "keyboard cat"
   })
 );
 
