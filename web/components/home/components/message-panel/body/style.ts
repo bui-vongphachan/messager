fa12ac514b999774aaa629290b96f1e001aa1724
Vongphachan Nukkalash <vongphachannukkalash@gmail.com>
2022-02-18 21:01:18 +0700

    เพี่ม margin ให้กล่องข้อความของฝ่ายตรงข้าม

--- web/components/home/components/message-panel/body/style.ts
+++ web/components/home/components/message-panel/body/style.ts
@@ -2,16 +2,6 @@ import { Theme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles((theme: Theme) => ({
-  /* box: {
-    flex: "1 1 0",
-    height: "100%",
-    display: "flex",
-    flexDirection: "column-reverse",
-    overflowX: "hidden",
-    overflowY: "scroll",
-    margin: "0.5rem 0",
-    backgroundColor: theme.palette.grey[100],
-  }, */
   box: {
     height: "100%",
     overflowX: "hidden",
