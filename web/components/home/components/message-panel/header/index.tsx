9d95bf5812e885d267a372bdee880a21d79e9c2b
Vongphachan Nukkalash <vongphachannukkalash@gmail.com>
2022-02-20 23:55:41 +0700

    ເພີ່ມປຸ່ມຍ້ອນກັບ

--- web/components/home/components/message-panel/header/index.tsx
+++ web/components/home/components/message-panel/header/index.tsx
@@ -1,11 +1,24 @@
-import { Avatar, Box, ListItemAvatar, TextField, Toolbar } from "@mui/material";
-import { useContext } from "react";
+import {
+  Avatar,
+  Box,
+  IconButton,
+  ListItemAvatar,
+  Toolbar,
+} from "@mui/material";
+import { useCallback, useContext } from "react";
 import { HomePageContext } from "../../../context";
 import { useStyles } from "./style";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 export default function MessagePanelHeader() {
-  const { selectedProfile } = useContext(HomePageContext);
+  const { selectedProfile, setSelectedProfile } = useContext(HomePageContext);
   const classes = useStyles();
+
+  const clearSelectedProfile = useCallback(() => {
+    if (setSelectedProfile) {
+      setSelectedProfile(null);
+    }
+  }, [setSelectedProfile]);
 
   if (!selectedProfile) {
     return <Toolbar className={classes.toolbar}>MessagePanelFooter</Toolbar>;
@@ -13,6 +26,12 @@ export default function MessagePanelHeader() {
 
   return (
     <Toolbar className={classes.toolbar}>
+      <Box className={classes.backIcon}>
+        <IconButton aria-label="delete" onClick={clearSelectedProfile}>
+          <ArrowBackIcon />
+        </IconButton>
+      </Box>
+
       <ListItemAvatar>
         <Avatar alt="Remy Sharp" src={selectedProfile?.picture} />
       </ListItemAvatar>
--- web/components/home/components/message-panel/header/style.ts
+++ web/components/home/components/message-panel/header/style.ts
@@ -2,6 +2,9 @@ import { Theme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles((theme: Theme) => ({
+  backIcon: {
+    marginRight: "1rem",
+  },
   toolbar: {
     border: "1px solid #e0e0e0",
     borderRadius: "0.5rem",
