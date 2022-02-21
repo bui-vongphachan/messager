fb6640553663dfe929df54165e9e485108264d75
Vongphachan Nukkalash <vongphachannukkalash@gmail.com>
2022-02-21 07:35:24 +0700

    ເພີ່ມເງື່ອນໄຂການສະແດງ content ຕາມຂະໜາດໜ້າຈໍ

--- web/components/home/index.tsx
+++ web/components/home/index.tsx
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
 import dynamic from "next/dynamic";
 import { Box, Container, Grid, Hidden } from "@mui/material";
-import HomePageContextProvider from "./context";
+import HomePageContextProvider, { HomePageContext } from "./context";
 import { useStyles } from "./style";
 import ProfileListPanel from "./components/profile-list-panel";
 
 const MessagePanel = dynamic(() => import("./components/message-panel"));
 
 export default function RootHomePageComponent() {
-  const classes = useStyles();
   return (
     <HomePageContextProvider>
-      <Container>
-        <Box className={classes.root}>
-          <Grid container spacing={2}>
-            <Grid className={classes.messageGrid} item md={8} sm={12} xs={12}>
-              <MessagePanel />
-            </Grid>
-            <Hidden mdDown>
-              <Grid className={classes.messageGrid} item xs={4}>
-                <ProfileListPanel />
-              </Grid>
-            </Hidden>
-          </Grid>
-        </Box>
-      </Container>
+      <Layout />
     </HomePageContextProvider>
   );
 }
+
+const Layout = () => {
+  const { selectedProfile } = useContext(HomePageContext);
+  const classes = useStyles();
+
+  const getCondition = useMemo(() => {
+    return !selectedProfile;
+  }, [selectedProfile]);
+
+  return (
+    <Container>
+      <Box className={classes.root}>
+        <Grid container spacing={2}>
+          <Hidden mdDown={getCondition}>
+            <Grid className={classes.messageGrid} item md={8} sm={12} xs={12}>
+              <MessagePanel />
+            </Grid>
+          </Hidden>
+          <Hidden mdDown={!!selectedProfile}>
+            <Grid className={classes.messageGrid} item md={4} sm={12} xs={12}>
+              <ProfileListPanel />
+            </Grid>
+          </Hidden>
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
