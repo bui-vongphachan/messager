8a979684144155de266ca90cd94d22a7b8de66b3
vongphachan nukkalash <vongphachannukkalash@gmail.com>
2022-04-24 07:41:23 +0700

    start logging after docker run

--- web/docker-run.sh
+++ web/docker-run.sh
@@ -1,1 +1,1 @@
-docker run -d --restart always -m 1000m --name web-messenger --network host -it --env-file .env web-messenger
+docker run --restart always -m 1000m --name web-messenger --network host -it --env-file .env web-messenger
